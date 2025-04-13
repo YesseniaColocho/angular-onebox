f0a1c6f5d9800a3d450d49f7f70c6c75775e21fc
YesseniaColocho <Colocho.yessenia@gmail.com>
2025-04-13 21:21:50 +0200

    home script

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  eventos: any[]
+
+  constructor(private httpClient: HttpClient){
+    this.eventos=[]
+    
+  }
+  ngOnInit(): void {
+    this.httpClient.get("assets/data/events.json").subscribe((data: any) => {
+      this.eventos = data;
+    })
+  }
 }
